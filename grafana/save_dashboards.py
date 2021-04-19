f4dffedb1821b2fd54916e7bea188a6b297d936d
Brian Sherwood <bsherwood@juniper.net>
2021-04-19 13:07:10 -0700

    working on save_dashboard

--- grafana/save_dashboards.py
+++ grafana/save_dashboards.py
@@ -1,17 +1,38 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 
 import requests
 import json
 
-server = "https://127.0.0.1:3000/grafana"
+bearer = "eyJrIjoiWnVjWGpscjVhTGQzamRva1RySmVoZU1uZlZFbVd4U28iLCJuIjoicXFxIiwiaWQiOjF9"
+server = "http://127.0.0.1:3000"
 
 # Example 1: Get default Home dashboard:
 url = server + "/api/dashboards/home"
 # To get the dashboard by uid
 # url = server + "/api/dashboards/uid/" + uid
 
-headers = {"Authorization":"Bearer eyJrIjoiWnVjWGpscjVhTGQzamRva1RySmVoZU1uZlZFbVd4U28iLCJuIjoicXFxIiwiaWQiOjF9"}
+headers = {"Authorization":"Bearer " + bearer}
 r = requests.get(url = url, headers = headers, verify=False)
-
+#r = requests.get(url = url, headers = headers)
+#
 print(r.json())
+print(r)
+
+print("====================")
+
+url = server + "/api/search?query=%"
+headers = {
+    "Authorization":"Bearer " + bearer,
+    "Content-Type":"application/json",
+    "Accept": "application/json"
+}
+r = requests.get(url = url, headers = headers, verify=False)
+for item in r.json():
+    if item['type'] == 'dash-db':
+        print(item) 
+        url = server + "/api/dashboards/uid/" + item['uid']
+        r = requests.get(url = url, headers = headers, verify=False)
+        print (r.json())
+
+
